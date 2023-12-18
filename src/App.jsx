a9fc74c9a1f53d281e22a0a9f883c76f3dba7194
Duke-Skygawker <bart3kk990@gmail.com>
2023-12-18 12:56:43 +0100

    useReducer template

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import UseReducerTemplate from "./UseReducerTemplate";
+
+function App() {
+  return (
+    <div className="container">
+      <UseReducerTemplate />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/UseReducerTemplate.jsx
@@ -0,0 +1,73 @@
+import { useReducer } from "react";
+import { data } from "./data";
+
+const defaultState = {
+  people: data,
+  isLoading: false,
+};
+
+const CLEAR_LIST = "CLEAR_LIST";
+const RESET_LIST = "RESET_LIST";
+const REMOVE_ITEM = "REMOVE_ITEM";
+
+const reducer = (state, action) => {
+  if (action.type === CLEAR_LIST) {
+    return { ...state, people: [] };
+  }
+  if (action.type === RESET_LIST) {
+    return { ...state, people: data };
+  }
+  if (action.type === REMOVE_ITEM) {
+    let newPeople = state.people.filter(
+      (person) => person.id !== action.payload.id
+    );
+    return { ...state, people: newPeople };
+  }
+  throw new Error(`No matching "${action.type}" - action type`);
+};
+
+const UseReducerTemplate = () => {
+  const [state, dispatch] = useReducer(reducer, defaultState);
+
+  const removeItem = (id) => {
+    dispatch({ type: REMOVE_ITEM, payload: { id: id } });
+  };
+  const clearList = () => {
+    dispatch({ type: CLEAR_LIST });
+  };
+  const resetList = () => {
+    dispatch({ type: RESET_LIST });
+  };
+  return (
+    <div>
+      {state.people.map((person) => {
+        const { id, name } = person;
+        return (
+          <div key={id} className="item">
+            <h4>{name}</h4>
+            <button onClick={() => removeItem(id)}>remove</button>
+          </div>
+        );
+      })}
+      {state.people.length <= 0 ? (
+        <button
+          className="btn"
+          style={{ marginTop: "2rem" }}
+          onClick={resetList}
+        >
+          reset items
+        </button>
+      ) : (
+        <button
+          className="btn"
+          style={{ marginTop: "2rem" }}
+          onClick={clearList}
+        >
+          clear items
+        </button>
+      )}
+    </div>
+  );
+};
+
+export default UseReducerTemplate;
